af6194afaaaddcf85268c304ab8da60cf1162de6
Benjamin Wu <benjaminwu1337@gmail.com>
2020-01-25 17:05:23 -0500

    Completed CodePath prework

--- BenjaminWuApp/ViewController.swift
+++ BenjaminWuApp/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var backgroundColor: UIColor!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
     }
 
 
+    @IBAction func didTapButton(_ sender: Any) {
+        print("Hello")
+        textLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        //textLabel.text = "Bye from Benjamin Wu "
+        textLabel.text = textField.text
+        textField.text = ""
+        view.endEditing(true)
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello from Benjamin Wu"
+        view.backgroundColor = backgroundColor
+        textLabel.textColor = UIColor.black
+        
+    }
 }
 
